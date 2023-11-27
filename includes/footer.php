1710d412b5e2df95e026bea196c219b00163e361
Romain <91896586+RomainPro62@users.noreply.github.com>
2023-11-27 13:55:49 +0100

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -26,6 +26,6 @@
             <h6 class="copyright">&copy; 2023 Fripouilles, Inc, All rights reserved.</h6>
         </div>
     </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="" crossorigin="anonymous"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </body>
 </html>
